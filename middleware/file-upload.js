59f1d89b12a6a292e8c4c1a1b47492a3a3666c84
nderimsali096 <63560554+nderimsali096@users.noreply.github.com>
2021-10-02 17:26:59 +0200

    prepared for deployment

---
+++ middleware/file-upload.js
@@ -0,0 +1,29 @@
+const { Error } = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const MIME_TYPE_MAP = {
+	'image/png': 'png',
+	'image/jpeg': 'jpeg',
+	'image/jpg': 'jpg',
+};
+
+const fileUpload = multer({
+	limits: 500000,
+	storage: multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, 'uploads/images');
+		},
+		filename: (req, file, cb) => {
+			const extension = MIME_TYPE_MAP[file.mimetype];
+			cb(null, uuidv4() + '.' + extension);
+		},
+	}),
+	fileFilter: (req, file, cb) => {
+		const isValid = !!MIME_TYPE_MAP[file.mimetype];
+		let error = isValid ? null : new Error('Invalid mimetype!');
+		cb(error, isValid);
+	},
+});
+
+module.exports = fileUpload;
